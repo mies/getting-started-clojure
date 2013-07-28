1d0f7d805dae14008b1c511755d86f8fdf3af040
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2013-07-28 14:12:52 +0200

    init

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject getting-started-clojure "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring/ring-jetty-adapter "1.1.6"]]
+                 :uberjar-name "getting-started-clojure-standalone.jar"
+                 :min-lein-version "2.0.0")
+                 )
